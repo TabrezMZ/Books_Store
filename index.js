1bacea49dcbf4383a2a17a836bde0e743620a7b6
tabrezMZ <tabrez.mansuri4u@gmail.com>
2022-11-26 23:51:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+function openMenu() {
+    document.body.classList += " menu--open"
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu--open')
+}
+
+function openForm() {
+    document.body.classList += " form--open"
+}
+
+function closeForm() {
+    document.body.classList.remove('form--open')
+}
